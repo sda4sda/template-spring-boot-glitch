70c4607c9aea7ca2ffb09c8f638bc6445240a9dc
Daniel Scholtus <sdeonline@gmail.com>
2022-02-11 04:29:15 -0300

    Initial commit

---
+++ src/main/java/org/sda4sda/beliefsapp/svc/AdventistsBeliefsDomainServiceApplication.java
@@ -0,0 +1,13 @@
+package org.sda4sda.beliefsapp.svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventistsBeliefsDomainServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventistsBeliefsDomainServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/sda4sda/beliefsapp/svc/GreetingController.java
@@ -0,0 +1,21 @@
+package org.sda4sda.beliefsapp.svc;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Mono<Greeting> greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return Mono.just(new Greeting(counter.incrementAndGet(), String.format(template, name)));
+	}
+}
