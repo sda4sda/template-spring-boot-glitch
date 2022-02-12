ebbfb9faf620419a980d277006e3d521a35375eb
Daniel Scholtus <sdeonline@gmail.com>
2022-02-12 17:50:23 -0300

    Update and rename AdventistsBeliefsDomainServiceApplication.java to MySpringBootApplication.java

--- src/main/java/org/sda4sda/beliefs/svc/MySpringBootApplication.java
+++ src/main/java/org/sda4sda/beliefs/svc/MySpringBootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdventistsBeliefsDomainServiceApplication {
+public class MySpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdventistsBeliefsDomainServiceApplication.class, args);
+		SpringApplication.run(MySpringBootApplication.class, args);
 	}
 
 }
