6c1289a77d7d3366c33a5f25e6ed2d541721f267
Daniel Scholtus <sdeonline@gmail.com>
2022-02-12 17:51:01 -0300

    Update MySpringBootApplication.java

--- src/main/java/org/sda4sda/beliefs/svc/MySpringBootApplication.java
+++ src/main/java/org/sda4sda/beliefs/svc/MySpringBootApplication.java
@@ -1,4 +1,4 @@
-package org.sda4sda.beliefs.svc;
+package org.sda4sda.templates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
